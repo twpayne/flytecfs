4c2ffd8c14320862edb2f12329ac5b17cd4913af
Tom Payne <twpayne@gmail.com>
2008-10-28 00:47:33 +0100

    Write routes.gpx.

--- test_flytec.py
+++ test_flytec.py
@@ -5,9 +5,13 @@ import flytecfs
 import flytecproxy
 
 logging.basicConfig(level=logging.INFO)
-io = flytec.POSIXSerialIO('/dev/ttyUSB0')
+io = flytec.POSIXSerialIO('/dev/ttyUSB1')
 f = flytec.Flytec(io)
 fp = flytecproxy.FlytecProxy(f)
+rd = flytecfs.RoutesFile(fp)
+file = open('routes.gpx', 'w')
+file.write(rd.read(1024 * 1024, 0))
+file.close()
 rd = flytecfs.TracklogsZipFile(fp)
 file = open('tracks.zip', 'w')
 file.write(rd.read(1024 * 1024, 0))
